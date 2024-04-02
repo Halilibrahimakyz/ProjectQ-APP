533c1fe557803a6e426ebe384b59998de4938944
Halil İbrahim AKYÜZ <58sbag58@gmail.com>
2024-04-02 19:26:00 +0300

    added svg modules

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,4 @@
-import {Button,Text, View} from 'react-native';
+import {Button,ScrollView,Text, View} from 'react-native';
 import React from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
@@ -7,11 +7,15 @@ import { setDarkTheme, setLightTheme } from '../../storeReduxToolkit/themeSlice'
 import { loginSuccess, logout } from '../../storeReduxToolkit/userDonorSlice';
 import {useTheme} from '@/constants/Colors';
 import {useLanguage} from '@/constants/language'
+import Welcome from '../../assets/svg/Welcome.svg'
+import Dark from '@/assets/svg/Dark.svg'
+import Light from '@/assets/svg/Light.svg'
 
 const HomeScreen = props => {
 
   const counterNum = useSelector((state) => state.counter.value);
   const language= useSelector((state) => state.language.value);
+  const theme= useSelector((state) => state.theme.value);
   const userDonor= useSelector((state) => state.userDonor);
   const userStudent= useSelector((state) => state.userStudent.userInfo);
   console.log("userDonor: ",userDonor)
@@ -20,11 +24,17 @@ const HomeScreen = props => {
   const dispatch = useDispatch();
 
   return (
-    <View style={{flex: 1,backgroundColor:useTheme().background, width: "100%", height: "100%"}}>
+    <ScrollView style={{flex: 1,backgroundColor:useTheme().background, width: "100%", height: "100%"}}>
         <Text style={{color:useTheme().primary,fontSize:34}}>Counter: {counterNum} (using redux and persist)</Text>
         <Text style={{color:useTheme().primary,fontSize:34}}>Language: {language} {getVal("welcome")}</Text>
         <Text style={{color:useTheme().primary,fontSize:34}}>userDonor: {userDonor.userInfo?.name} </Text>
         <Text style={{color:useTheme().primary,fontSize:34}}>isAuthenticated: {userDonor.isAuthenticated ? "true": "false"} </Text>
+        <View style={{alignItems:'center'}}>
+          {theme === 'dark'?(<Dark  width={300} height={300} />):(<Light  width={300} height={300} />)
+
+          }
+        
+        </View>
         <Button title="Increment" onPress={() => dispatch(increment())} />
         <Button title="Decrement" onPress={() => dispatch(decrement())} />
         <Button title="setTurkish" onPress={() => changeLanguage('tr')} />
@@ -33,7 +43,7 @@ const HomeScreen = props => {
         <Button title="setLightTheme" onPress={() => dispatch(setLightTheme())} />
         <Button title="loginSuccess" onPress={() => dispatch(loginSuccess({name:"test",surname:"test2"}))} />
         <Button title="logout" onPress={() => dispatch(logout())} />
-        </View>
+        </ScrollView>
   );
 };
 
