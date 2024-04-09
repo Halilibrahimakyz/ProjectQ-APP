cba032f16e675af57c0606385c14b69393c3fd57
Halil İbrahim AKYÜZ <58sbag58@gmail.com>
2024-04-09 11:20:37 +0300

    Update Colors.js

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,39 +1,41 @@
 
 import { useSelector } from 'react-redux';
 
+const baseTheme = {
+  fontSize: {
+    small: 12, // Yardımcı metinler için, örneğin etiketler veya talimatlar.
+    body: 14, // Ana içerik metni için standart boyut. Paragraflar, açıklamalar.
+    button: 16, // Buton metinleri için. Yeterli dokunma alanı sağlamak ve okunabilirliği artırmak için.
+    title: 20, // Küçük başlıklar için. Bölüm başlıkları gibi.
+    heading: 24, // Büyük başlıklar için. Ekran başlıkları, önemli bilgiler.
+    display1: 30, // Büyük ekran başlıkları veya önemli metinler için.
+    display2: 36, // Çok büyük metinler için, örneğin açılış ekranında kullanılan metinler.
+  },
+  primary: '#13B156',
+  accent: '#09101D',
+  black: '#151619',
+  white: '#FFFFFF',
+};
+
 export const lightTheme = {
-  background: '#EBEEF2',//darkwhite
-  background2: '#151619',//black
-  primary: '#13B156',//green
-  secondary: '#262934',//grey
-  accent: '#09101D',//darkblue
-  lightGrey:'#838383',//lightGrey
-  black: '#151619',//black
-  white: '#FFFFFF' //white
+  ...baseTheme,
+  background: '#EBEEF2',
+  background2: '#151619',
+  secondary: '#262934',
+  lightGrey: '#838383',
+  type:'light'
 };
 
 export const darkTheme = {
-  background: '#151619',//black
-  background2: '#EBEEF2',//darkwhite
-  primary: '#13B156',//green
-  secondary: '#838383',//grey
-  lightGrey:'#838383',//lightGrey
-  accent: '#09101D',//darkblue
-  black: '#151619',//black
-  white: '#FFFFFF' //white
+  ...baseTheme,
+  background: '#151619',
+  background2: '#EBEEF2',
+  secondary: '#838383', // Assuming this was intended to be different in dark mode
+  lightGrey: '#838383',
+  type:'dark'
 };
-
 
 export const useTheme = () => {
-  const currentTheme = useSelector((state) => state.theme.value); 
-  const theme = currentTheme === 'light' ? lightTheme : darkTheme;
+  const theme = useSelector(state => state.theme.value === 'light' ? lightTheme : darkTheme);
   return theme;
 };
-
-// text: 'red', //light_orange
-// white:'#FFFFFF',
-// darkWhite:'#EBEEF2',
-// green:'#13B156',
-// gray:'#262934',
-// darkBlue:'#09101D',
-// black:'#151619'
