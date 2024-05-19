70748ab50107f9eae3146640098c4edfed8b9b77
Halil İbrahim AKYÜZ <58sbag58@gmail.com>
2024-05-19 15:37:11 +0300

    added local components folders

--- src/screens/index.js
+++ src/screens/index.js
@@ -6,7 +6,6 @@ import AppInfoSupporterScreen from './onBoard/appInfoSupporter';
 import JoinScreen from './onBoard/join';
 
 //Student
-import testStudentPageScreen from './student/testStudentPage';
 import StudentHomeScreen from './student/studentHome'
 import StudentCreateScreen from './student/studentCreate';
 import StudentListScreen from './student/studentList';
@@ -14,23 +13,20 @@ import StudentMessagesScreen from './student/studentMessages';
 import StudentProfileScreen from './student/studentProfile';
 
 //Supporter
-import testSupporterPageScreen from './supporter/testSupporterPage';
 import SupporterHomeScreen from './supporter/supporterHome';
 import SupporterHistoryScreen from './supporter/supporterHistory';
 import SupporterListScreen from './supporter/supporterList';
 import SupporterMessagesScreen from './supporter/supporterMessages';
 import SupporterProfileScreen from './supporter/supporterProfile';
+
 //Other
-import HomeScreen from './home';
+
 
 export {
     WelcomeScreen,
     OnBoardScreen,
     AppInfoStudentScreen,
     AppInfoSupporterScreen,
-    testStudentPageScreen,
-    testSupporterPageScreen,
-    HomeScreen,
     JoinScreen,
     StudentHomeScreen,
     StudentCreateScreen,
