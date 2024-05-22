f680f89d01d53c80046bbead4835fac1bdef848c
ardacan271 <ardacan271@gmail.com>
2024-05-22 23:07:43 +0300

    created BalanceDisplay for wallet

--- src/screens/student/studentWalletScreen/index.js
+++ src/screens/student/studentWalletScreen/index.js
@@ -1,9 +1,11 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, ScrollView, View } from 'react-native';
 import React, { useMemo } from 'react';
 import { useTheme } from '@/constants/colors';
 import { useLanguage } from '@/constants/language'
 import { Container } from '@/components';
 import { popScreen, pushScreen } from '@/navigation/navigationFunctions';
+
+import BalanceDisplay from './components/BalanceDisplay';
 
 const StudentWalletScreen = props => {
 
@@ -13,7 +15,7 @@ const StudentWalletScreen = props => {
 
   return (
     <Container style={styles.container} topBarProps={{
-      title: 'Wallet',
+      title: getVal("wallet"),
       onLeftPress: () => { popScreen(props.componentId); },
       leftIcon: 'arrow-left',
       onRightPress: () => { console.log('Sağ tıklandı'); },
@@ -22,7 +24,13 @@ const StudentWalletScreen = props => {
     }}
       compId={props.componentId}
     >
-      <Text style={{ color: theme.primary }}>StudentWalletScreen</Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={styles.scroller}
+        contentContainerStyle={{alignItems: "center"}}
+      >
+        <BalanceDisplay balance={349}/>
+      </ScrollView>
     </Container>
   );
 };
@@ -31,6 +39,18 @@ const getStyles = (theme) => StyleSheet.create({
   container: {
     justifyContent: 'space-between',
   },
+  scroller: {
+    flex: 1,
+    backgroundColor: theme.background,
+    width: "100%",
+    height: "100%"
+  },
+  horizontalRuler: {
+    height: StyleSheet.hairlineWidth,
+    width: "100%",
+    backgroundColor: theme.lightGrey,
+    marginTop: 16
+  }
 });
 
 export default StudentWalletScreen;
